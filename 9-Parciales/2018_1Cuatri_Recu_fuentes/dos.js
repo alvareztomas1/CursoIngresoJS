2a4c927233ddc888a4c4b3bd86d1041577592b5a
alvareztomas1 <alvareztomas1@hotmail.com>
2019-02-15 09:55:33 -0300

    ejercicio2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
+    var precio1,descuento;
+    precio1 = document.getElementById("elNombre").value;
+    descuento = (precio1-(precio1*10)/100);
+    iva = ((descuento*21)/100+descuento)
+    alert ("tu compra es de "+"$"+precio1+" tenes un descuento del 10% "+" queda en"+" $"+descuento+" mas el iva es "+"$ "+iva);
   
 }

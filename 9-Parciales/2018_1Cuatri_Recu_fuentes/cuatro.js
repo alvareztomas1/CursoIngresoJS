c56b33fe8be98461f5dfeb00c8001b8383941700
alvareztomas1 <alvareztomas1@hotmail.com>
2019-02-15 11:04:55 -0300

    punto 3 y 4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+    var libros,precio,tarjeta,resultado,resultado1;
+    libros = prompt ("cuantos libros va a comprar");
+    precio = prompt ("cual es el total");
+    tarjeta = prompt ("paga con tarjeta?");
+    if (libros>2) 
+            {resultado = (precio-(precio*10)/100);
+            alert ("su precio es "+resultado);}
+        else
+        {
+            if (precio>2000)
+                {resultado1 = (precio-(precio*15)/100);
+                alert ("su precio es "+ resultado1);}
+            else
+                alert ("su precio es "+precio);
+                
+        }
+        
+    
+
+    
 
 }
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,9 +1,19 @@
 function mostrar()
 {
-    var precioFinal,propina;
+    var precioFinal,propina,resultado,chicos;
+    
+    chicos = prompt ("cuantos chicos son")
     precioFinal = document.getElementById("elPrecioFinal").value;
     precioFinal = parseInt (precioFinal);
+
     propina = ((precioFinal*10)/100);
-    resultado = ((precioFinal/3)+propina);
-    alert ("el precio es de "+precioFinal+"$ "+" mas la propina que son "+propina+"$"+" cada uno tiene que poner "+resultado+" pesos");
+    propina = parseInt (propina);
+    
+    precio = (precioFinal+propina);
+    precio = parseInt (precio);
+
+    resultado = ((precioFinal+propina)/chicos);
+    
+
+    alert ("el precio es de "+precioFinal+"$ "+" mas la propina que son "+propina+"$"+" el precio seria "+precio+"$ "+" cada uno tiene que poner "+resultado+" pesos");
 }

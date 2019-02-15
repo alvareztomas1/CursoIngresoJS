5ba5e28b5ce087c940b2168e1159765a6007e82a
alvareztomas1 <alvareztomas1@hotmail.com>
2019-02-15 10:22:02 -0300

    punto 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -2,6 +2,7 @@ function mostrar()
 {
     var precio1,descuento;
     precio1 = document.getElementById("elNombre").value;
+    precio1 = parseInt (precio1);
     descuento = (precio1-(precio1*10)/100);
     iva = ((descuento*21)/100+descuento)
     alert ("tu compra es de "+"$"+precio1+" tenes un descuento del 10% "+" queda en"+" $"+descuento+" mas el iva es "+"$ "+iva);
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
-
+    var precioFinal,propina;
+    precioFinal = document.getElementById("elPrecioFinal").value;
+    precioFinal = parseInt (precioFinal);
+    propina = ((precioFinal*10)/100);
+    resultado = ((precioFinal/3)+propina);
+    alert ("el precio es de "+precioFinal+"$ "+" mas la propina que son "+propina+"$"+" cada uno tiene que poner "+resultado+" pesos");
 }

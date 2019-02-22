d3198b6db31b8572b66bda0fddd1b336815c19d6
alvareztomas1 <alvareztomas1@hotmail.com>
2019-02-21 21:04:33 -0300

    jueves 21/02 21 horas

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -12,17 +12,72 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 var numeroSecreto; 
 var contadorIntentos;
 
+
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
 	
+	numeroSecreto = Math.floor((Math.random() * 100) + 1);
+	console.log(numeroSecreto);
+	contadorIntentos=0
+	//Genero el número RANDOM entre 1 y 100
 
 }
 
 function verificar()
 {
+	var numeroIngresado;
+	var alerta;
+	numeroIngresado = document.getElementById("numero").value;
+	numeroIngresado = parseInt (numeroIngresado);
+	contadorIntentos=contadorIntentos+1;
+	
+	console.log(contadorIntentos);
 	
 	
-
+	if (numeroIngresado == numeroSecreto)
+	{
+	switch (contadorIntentos)
+	{
+		case 1:
+			alerta = ("usted es un psiquico");
+			break;
+		case 2:
+			alerta = ("buena percepcion");
+			break;
+		case 3:
+			alerta = ("pura suerte");
+			break;
+		case 4:
+			alerta = ("excelente tecnica")
+			break;
+		case 5:
+			alerta = ("usted esta en la media")
+			break;
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			alerta = ("falta tecnica");
+			break;
+		default:
+			alerta = ("afortunado en el amor!!");
+			break;
+		}
+		
+	}
+	else
+	{
+		if (numeroIngresado>numeroSecreto)
+		{
+			alert ("se paso");
+		}
+		else
+		{
+			alert ("le falta");
+		}
+	}
+	alert(alerta)
+	document.getElementById("intentos").value= contadorIntentos;
+	
 }

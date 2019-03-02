974e8082e7d4087b41b2015bb8a18219b5b9ffe9
alvareztomas1 <alvareztomas1@hotmail.com>
2019-03-02 16:10:24 -0300

    tercer practica

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,6 +1,88 @@
 function mostrar()
 {
-    var pais;
+    var nombre;
+    var tema;
+    var paginas;
+    var ventas;
+    var contador;
+    var respuesta;
+    var contadorImpares;
+    var contadorCeros;
+    var sumaVentas;
+    var promedio;
+    var sumaProgramacion;
+//--------------------------------------------------------
+    contador=0;
+    paginas=0;
+    contadorPares=0;
+    contadorImpares=0;
+    ventas=0;
+    contadorCeros=0;
+    sumaVentas=0;
+    sumaProgramacion=0;
+//--------------------------------------------------------
+    while (respuesta!="no")
+    {   
+        nombre = prompt ("ingrese el nombre del libro");
+        paginas = prompt ("ingrese la cantidad de paginas del libro");
+        paginas = parseInt(paginas);
+        ventas = prompt ("ingrese la cantidad de ventas");
+        ventas = parseInt(ventas);
+        tema = prompt ("de que tema es el libro");
+
+        if (paginas%2==0 && paginas!=0)
+        {
+            contadorPares++;
+        }
+        if (paginas%3==0 && paginas!=0)
+        {
+            contadorImpares++;
+        }
+        if (ventas==0)
+        {
+            contadorCeros++;
+        }
+        if (tema=="programacion")
+        {
+            sumaProgramacion=(sumaProgramacion+paginas);
+        }
+        sumaVentas=(sumaVentas+ventas);
+        contador++;
+        respuesta=prompt("ingrese no par salir");
+    }
+//--------------------------------------------------------
+    promedio = (sumaVentas/contador);
+    alert ("los libros con paginas pares son: "+contadorPares);
+    alert ("los libros con paginas impares son: "+contadorImpares);
+    alert ("los libros que tuvieron cero ventas fueron: "+contadorCeros);
+    alert ("el promedio de ventas fue "+promedio);
+    alert ("los libros de programacion suman "+sumaProgramacion+" paginas");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var pais;
     var superficie
     var respuesta;
     var cantidadImpar;
@@ -63,4 +145,4 @@ function mostrar()
     alert ("el primer pais que supero 100 de superficie fue "+primerPais);
     alert ("el promedio de las superficies es "+promedio);
     alert ("el pais con menor superficie es "+ paisBajo+" , con "+superficieBaja);
-}
+    */
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,58 +1,151 @@
 function mostrar()
 {
-    var velocidad;
-    var combustible;
+    var nota;
+    var sexo;
+    var edad;
     var contador;
+    var sumaNota;
     var promedio;
-    var sumaVelocidad;
-    var velocidadBaja;
-    var contadorVelocidad;
-
-
+    var notaMinima;
+    var sexoDeLaNota;
+    var contadorVarones;
+    var edadMinima;
+    var primerMujer;
+    var edadPrimera;
+    var notaPrimera;
+//-------------------------------------------------------------------
     contador=0;
-    sumaVelocidad=0;
-    velocidadBaja=0;
-    contadorVelocidad=0;
-    velocidadAlta=0;
-    
-
-
+    sumaNota=0;
+    notaMinima=0;
+    contadorVarones=0;
+    edadMinima=0;
+    primerMujer="";
+//-------------------------------------------------------------------
     while (contador<5)
     {
-        velocidad = prompt ("ingrese la velocidad entre 0 y 250");
-        velocidad = parseInt (velocidad);
+        nota = prompt ("ingrese su nota");
+        nota = parseInt (nota);
+        sexo = prompt ("ingrese m o f segun su sexo");
+        edad = prompt ("ingrese su edad");
+        edad = parseInt (edad);
+
+        if (nota>10 || nota <0)
+        {
+            prompt ("las notas deben estar entre 10 y 0. Reingrese");
+        }
+        if (nota<notaMinima || contador==0)
+        {
+            notaMinima=nota;
+            sexoDeLaNota=sexo;
+        }
+        if (edad>18 && nota>=6)
+        {
+            contadorVarones++;
+        }
+        if (edad<edadMinima || contador==0)
+        {
+            edadMinima=edad;
+            sexoJoven=sexo;
+            notaJoven=nota;
+        }
+        if (sexo=="f" && primerMujer=="")
+        {
+            primerMujer=sexo;
+            edadPrimera=edad;
+            notaPrimera=nota;
+        }
         
-        if (velocidad<0 || velocidad>250)
-        {
-            alert ("las velocidades tienen que ser entre 0 y 250");
-        }
-        combustible = prompt ("ingrese l o s segun el tipo de combustible");
-        if (combustible != "l" && combustible != "s")
-        {
-            alert ("el combustible tiene que ser s o l");
-        }
-        if (velocidad<=velocidadBaja || contador==0)
-        {
-            velocidadBaja=velocidad;
-            combustibleBajo=combustible;
-        }
-        if (combustible=="l" && velocidad>100)
-        {
-            contadorVelocidad++;
-        }
-        if (velocidad>=velocidadAlta && combustible=="s" || contador==0)
-        {
-            velocidadAlta=velocidad;
-        }
 
 
-        sumaVelocidad= (sumaVelocidad+velocidad);
+
+
+        sumaNota = (sumaNota+nota);
         contador++;
     }
-    promedio=(sumaVelocidad/5)
-    
-    alert ("el promedio de velocidades es "+promedio);
-    alert ("la velocidad mas baja fue "+velocidadBaja+" y su tipo de combustible fue "+combustibleBajo);
-    alert ("hubo "+contadorVelocidad+" combustibles liquidos que superaron los 100 km");
-    alert ("la velocidad mas alta de combustible solido es "+velocidadAlta);
+//-------------------------------------------------------------------
+    if (sexo!="f")
+    {
+        alert ("no hubo ninguna mujer");
+    }
+
+    promedio = (sumaNota/5);
+    alert ("el promedio de las notas es "+promedio);
+    alert ("la nota mas baja fue "+notaMinima+" y el sexo es "+sexoDeLaNota);
+    alert ("la nota del mas joven fue "+notaJoven+" y su sexo es "+sexoJoven);
+    alert ("la primer mujer se saco "+notaPrimera+" y tiene "+edadPrimera+" años");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+var velocidad;
+var combustible;
+var contador;
+var promedio;
+var sumaVelocidad;
+var velocidadBaja;
+var contadorVelocidad;
+
+
+contador=0;
+sumaVelocidad=0;
+velocidadBaja=0;
+contadorVelocidad=0;
+velocidadAlta=0;
+
+
+
+while (contador<5)
+{
+    velocidad = prompt ("ingrese la velocidad entre 0 y 250");
+    velocidad = parseInt (velocidad);
+    
+    if (velocidad<0 || velocidad>250)
+    {
+        alert ("las velocidades tienen que ser entre 0 y 250");
+    }
+    combustible = prompt ("ingrese l o s segun el tipo de combustible");
+    if (combustible != "l" && combustible != "s")
+    {
+        alert ("el combustible tiene que ser s o l");
+    }
+    if (velocidad<=velocidadBaja || contador==0)
+    {
+        velocidadBaja=velocidad;
+        combustibleBajo=combustible;
+    }
+    if (combustible=="l" && velocidad>100)
+    {
+        contadorVelocidad++;
+    }
+    if (velocidad>=velocidadAlta && combustible=="s" || contador==0)
+    {
+        velocidadAlta=velocidad;
+    }
+
+
+    sumaVelocidad= (sumaVelocidad+velocidad);
+    contador++;
+}
+promedio=(sumaVelocidad/5)
+
+alert ("el promedio de velocidades es "+promedio);
+alert ("la velocidad mas baja fue "+velocidadBaja+" y su tipo de combustible fue "+combustibleBajo);
+alert ("hubo "+contadorVelocidad+" combustibles liquidos que superaron los 100 km");
+alert ("la velocidad mas alta de combustible solido es "+velocidadAlta);
+*/
